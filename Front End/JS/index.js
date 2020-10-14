128e585d41878a4dd181868c9838fc9c979f6b0c
reece-elder <RElder@academytrainee.com>
2020-10-14 10:33:23 +0100

    Added RenderData() to populate fields with Database data

--- Front End/JS/index.js
+++ Front End/JS/index.js
@@ -12,54 +12,9 @@ const submitButton = document.querySelector('#submitButton');
 
 const shipNameInput = document.querySelector('.ship-name');
 
+const output = document.querySelector('#logContent');
+
 function addRecord(){
-
-    let x = idNumber += 1;
-
-    let newRecord = document.createElement('div');
-    newRecord.setAttribute("class", "row m-0 ship-log");
-    newRecord.setAttribute("id","record-" + x);
-    newRecord.setAttribute("onclick","locateDelete(this)");
-
-    let newIDNumber = document.createElement('div');
-    newIDNumber.setAttribute("class", "col-1");
-    newIDNumber.innerHTML = '<h2 class="text-center">' + x + '</h2>';
-
-    let newShipName = document.createElement('div');
-    newShipName.setAttribute("class", "col-3");
-    newShipName.innerHTML = '<h2 class="text-center">ship name</h2>';
-
-    let newCaptainName = document.createElement('div');
-    newCaptainName.setAttribute("class", "col-2");
-    newCaptainName.innerHTML = '<h2 class="text-center">ship name</h2>';
-
-    let newClass = document.createElement('div');
-    newClass.setAttribute("class", "col-2");
-    newClass.innerHTML = '<h2 class="text-center">ship name</h2>';
-
-    let newOrigin = document.createElement('div');
-    newOrigin.setAttribute("class", "col-2");
-    newOrigin.innerHTML = '<h2 class="text-center">ship name</h2';
-
-    let newCargo = document.createElement('div');
-    newCargo.setAttribute("class", "col-2");
-    newCargo.innerHTML = '<h2 class="text-center">ship name</h2>';
-    //<div class="input-group justify-content-center"> <input type="text" class="cargo-name"> </div>
-
-    const output = document.querySelector('.main_content');
-    output.appendChild(newRecord);
-
-    newRecord.appendChild(newIDNumber);
-    newRecord.appendChild(newShipName);
-    newRecord.appendChild(newCaptainName);
-    newRecord.appendChild(newClass);
-    newRecord.appendChild(newOrigin);
-    newRecord.appendChild(newCargo);
-
-    fillRecord();
-}
-
-function fillRecord(){
 
 }
 
@@ -73,7 +28,7 @@ function locateDelete(element){
     }
 }
 
-function toggleActiveButtons(){
+function deleteLog(id){
     
 }
 
@@ -93,16 +48,17 @@ deleteButton.addEventListener("click", function(){
 });
 
 updateButton.addEventListener("click", function(){
-    if(updateButtonClick){
-        console.log("update button toggled off");
-        updateButtonClick = false;
-        updateButton.classList.remove("button-active");
-    } else {
-        console.log("update button toggled on");
-        updateButtonClick = true;
-        updateButton.classList.add("button-active");
-    }
-    console.log("Update button clicked");
+    // if(updateButtonClick){
+    //     console.log("update button toggled off");
+    //     updateButtonClick = false;
+    //     updateButton.classList.remove("button-active");
+    // } else {
+    //     console.log("update button toggled on");
+    //     updateButtonClick = true;
+    //     updateButton.classList.add("button-active");
+    // }
+    // console.log("Update button clicked");
+    renderData();
 });
 
 newEntryButton.addEventListener("click", function(){
@@ -133,4 +89,60 @@ submitButton.addEventListener("click", function(){
         submitButton.classList.add("button-active");
     }
 
-})
+});
+
+renderData();
+
+function renderData() {
+
+    output.innerHTML = "";
+
+    fetch("http://localhost:8082/getAll")
+        .then(response => response.json())
+        .then(logArray => {
+            logArray.forEach(function(log) {
+
+                let newRecord = document.createElement('div');
+                newRecord.setAttribute("class", "row m-0 ship-log");
+                newRecord.setAttribute("id","record-" + log.id);
+                newRecord.addEventListener("click", function() {
+                    deleteLog(log.id);
+                });
+                
+            
+                let newIDNumber = document.createElement('div');
+                newIDNumber.setAttribute("class", "col-1");
+                newIDNumber.innerHTML = '<h2 class="text-center">' + log.id + '</h2>';
+            
+                let newShipName = document.createElement('div');
+                newShipName.setAttribute("class", "col-3");
+                newShipName.innerHTML = '<h2 class="text-center">' + log.shipName + '</h2>';
+            
+                let newCaptainName = document.createElement('div');
+                newCaptainName.setAttribute("class", "col-2");
+                newCaptainName.innerHTML = '<h2 class="text-center">' + log.captainName + '</h2>';
+            
+                let newClass = document.createElement('div');
+                newClass.setAttribute("class", "col-2");
+                newClass.innerHTML = '<h2 class="text-center">' + log.shipClass + '</h2>';
+            
+                let newOrigin = document.createElement('div');
+                newOrigin.setAttribute("class", "col-2");
+                newOrigin.innerHTML = '<h2 class="text-center">' + log.origin + '</h2>';
+            
+                let newCargo = document.createElement('div');
+                newCargo.setAttribute("class", "col-2");
+                newCargo.innerHTML = '<h2 class="text-center">' + log.cargo + '</h2>';
+                //<div class="input-group justify-content-center"> <input type="text" class="cargo-name"> </div>
+
+                output.appendChild(newRecord);
+            
+                newRecord.appendChild(newIDNumber);
+                newRecord.appendChild(newShipName);
+                newRecord.appendChild(newCaptainName);
+                newRecord.appendChild(newClass);
+                newRecord.appendChild(newOrigin);
+                newRecord.appendChild(newCargo);
+            });
+        }).catch(error => console.error(error));
+}
